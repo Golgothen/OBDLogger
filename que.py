b2b3f28c17b4e4193df5bed519228c92c2c814d5
Paul Ellis <golgothen@gmail.com>
2017-05-26 16:26:04 +1000

    Bug fixed in Que

--- que.py
+++ que.py
@@ -5,7 +5,7 @@ import os, sys
 from threading import Thread
 from multiprocessing import Queue, Event
 from datetime import datetime
-from time import sleep
+from time import sleep, time
 from general import *
 
 
@@ -13,16 +13,15 @@ class Que(Thread):
 
     def __init__(self, name, frequency, que):
         super(Que,self).__init__()
-        self.__lastPolled = None
         self.__commands = dict()
         self.running = False
         self.__frequency = frequency
         self.__que = que
-        self.ready = False
-        self.paused = True
+        self.__ready = False
+        self.__paused = True
         self.pauseEvent = Event()
         self.resumeEvent = Event()
-        self.readyEvent = Event()
+        self.__readyEvent = Event()
         self.daemon=True
         self.name=name
         self.deleteAfterPoll = False
@@ -31,27 +30,25 @@ class Que(Thread):
 
     def run(self):
         self.running = True
-        self.__lastPolled = datetime.now()
         logger.info('Que {} starting'.format(self.name))
         while self.running:
             try:
-                self.__lastPolled = datetime.now()
-                if not self.ready:
+                if not self.__ready:
                     logger.debug('Not Ready on thread {}'.format(self.name))
-                    self.readyEvent.wait()
-                    self.readyEvent.clear()
+                    self.__readyEvent.wait()
+                    self.__readyEvent.clear()
                     logger.debug('Ready on thread {}'.format(self.name))
-                if not self.paused:
+                if not self.__paused:
                     for s in self.__commands:
+                        lastPolled = time()
                         logger.debug('Que {} adding command {} to output queue'.format(self.name, s))
                         self.__que.put(s)
                         if self.deleteAfterPoll:
                             self.removeCommand(s)
                             break
-                        if len(self.__commands)>0:
-                            sleep(1.0/self.__frequency/len(self.__commands))
-                        else:
-                            sleep(1.0/self.__frequency)
+                        time_elapsed = time() - lastPolled
+                        sleeptime = (1.0 / self.__frequency / len(self.__commands)) - time_elapsed
+                        sleep(sleeptime)
                 else:
                     logger.debug('Pausing thread {}'.format(self.name))
                     self.pauseEvent.set()
@@ -60,6 +57,8 @@ class Que(Thread):
                     logger.debug('Resuming thread {}'.format(self.name))
             except (KeyboardInterrupt, SystemExit):
                 self.running = False
+                continue
+            except (RuntimeError):
                 continue
             except:
                 logger.critical('Exception caught in Queue Thread {}:'.format(self.name), exc_info = True, stack_info = True)
@@ -72,18 +71,18 @@ class Que(Thread):
 
     def addCommand(self, command, override = False):
         logger.info('Appending command {} to que {}'.format(command, self.name))
-        pausedState = self.paused
-        if not self.paused:
-            self.paused = True                         # Pause the que when making changes
-            if self.ready:
+        pausedState = self.__paused
+        if not self.__paused:
+            self.__paused = True                         # Pause the que when making changes
+            if self.__ready:
                 self.pauseEvent.wait()                      # Wait for pauseWait event to ensure dict will not be accessed
                 self.pauseEvent.clear()
         self.__commands[command]=override
-        if not self.ready:
-            self.readyEvent.set()
-        self.ready = True
+        if not self.__ready:
+            self.__readyEvent.set()
+        self.__ready = True
         if not pausedState:
-            self.paused=False                          # Resume que after update
+            self.__paused=False                          # Resume que after update
         self.resumeEvent.set()
 
     def getCommands(self):
@@ -94,16 +93,23 @@ class Que(Thread):
 
     def removeCommand(self, command):
         logger.debug('Removing sensor {} from que {}'.format(command, self.name))
-        self.paused = True
-        self.pauseEvent.wait()
-        self.pauseEvent.clear()
+        pausedState = self.__paused
+        if not self.__paused:
+            self.__paused = True
+            if self.__ready:
+                logger.debug('Waiting for main loop to stop on que {}'.format(self.name))
+                self.pauseEvent.wait()
+                self.pauseEvent.clear()
+        logger.debug('Main loop stopped on que {}'.format(self.name))
         if command in self.__commands:
             del self.__commands[command]
             if len(self.__commands)==0:
-                self.ready=False
+                self.__ready=False
                 logger.info('Que {} not ready due to zero length'.format(self.name))
-        self.paused = False
+        if not pausedState:
+            self.__paused = False
         self.resumeEvent.set()
+        logger.debug('Resuming main loop on que {}'.format(self.name))
 
     def status(self):
         #returns a dict of que status
@@ -111,7 +117,14 @@ class Que(Thread):
         d['Name']=self.name
         d['Frequency']=self.__frequency
         d['Running']=self.running
-        d['Ready']=self.ready
-        d['Paused']=self.paused
+        d['Ready']=self.__ready
+        d['Paused']=self.__paused
         d['Length']=len(self.__commands)
         return d
+
+    def pause(self):
+        self.__paused = True
+
+    def resume(self):
+        self.__paused = False
+        self.resumeEvent.set()
